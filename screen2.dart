09606b01c4a0844f04383129281adb82c748ede3
karthikmohan409 <karthikmohan409@gmail.com>
2024-05-30 17:25:40 -0400

    Create screen2.dart

---
+++ screen2.dart
@@ -0,0 +1,166 @@
+      // Figma Flutter Generator Iphone1415promax2Widget - FRAME
+      Container(
+      width: 430,
+      height: 932,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(251, 251, 251, 1),
+      gradient : LinearGradient(
+          begin: Alignment(7.782464450656335e-9,-1.0542986392974854),
+          end: Alignment(1.0542986392974854,3.2185917918771487e-16),
+          colors: [Color.fromRGBO(65, 22, 156, 1),Color.fromRGBO(50, 49, 116, 1)]
+        ),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 574,
+        left: 34,
+        child: Text('Easy booking', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'GT Walsheim Pro',
+        fontSize: 28,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 616,
+        left: 34,
+        child: Text('Effortlessly reserve indoor sports facilities with our app. Enjoy quick and hassle-free bookings for your favorite activities.', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.800000011920929),
+        fontFamily: 'GT Walsheim Pro',
+        fontSize: 18,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1.5 /*PERCENT not supported*/
+      ),)
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/rectangle1.svg',
+        semanticsLabel: 'rectangle1'
+      );
+      ),Positioned(
+        top: 756,
+        left: 33,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+            bottomLeft: Radius.circular(40),
+            bottomRight: Radius.circular(40),
+          ),
+      color : Color.fromRGBO(255, 198, 0, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 140, vertical: 15),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Continue', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'GT Walsheim Pro',
+        fontSize: 20,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1.5 /*PERCENT not supported*/
+      ),),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 525,
+        left: 37,
+        child: Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+        width: 37,
+        height: 11,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(5.5),
+            topRight: Radius.circular(5.5),
+            bottomLeft: Radius.circular(5.5),
+            bottomRight: Radius.circular(5.5),
+          ),
+      color : Color.fromRGBO(255, 198, 0, 1),
+  )
+      ), SizedBox(width : 6),
+Container(
+        width: 11,
+        height: 11,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(5.5),
+            topRight: Radius.circular(5.5),
+            bottomLeft: Radius.circular(5.5),
+            bottomRight: Radius.circular(5.5),
+          ),
+      color : Color.fromRGBO(255, 198, 0, 0.4000000059604645),
+  )
+      ), SizedBox(width : 6),
+Container(
+        width: 11,
+        height: 11,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(5.5),
+            topRight: Radius.circular(5.5),
+            bottomLeft: Radius.circular(5.5),
+            bottomRight: Radius.circular(5.5),
+          ),
+      color : Color.fromRGBO(255, 198, 0, 0.4000000059604645),
+  )
+      ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 29,
+        left: 331,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+      color : Color.fromRGBO(57, 36, 135, 0.15000000596046448),
+      border : Border.all(
+          color: Color.fromRGBO(0, 0, 0, 1),
+          width: 1,
+        ),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Skip', textAlign: TextAlign.right, style: TextStyle(
+        color: Color.fromRGBO(57, 36, 135, 1),
+        fontFamily: 'GT Walsheim Pro',
+        fontSize: 18,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1.5 /*PERCENT not supported*/
+      ),),
+
+        ],
+      ),
+    )
+      ),
+        ]
+      )
+    )
