07560d0f282483cc5319e1ba2b29ecb60e94bbff
karthikmohan409 <karthikmohan409@gmail.com>
2024-05-30 17:34:00 -0400

    Create screen6.dart

---
+++ screen6.dart
@@ -0,0 +1,33 @@
+      // Figma Flutter Generator Iphone1415promax6Widget - FRAME
+      Container(
+      width: 430,
+      height: 932,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(251, 251, 251, 1),
+      gradient : LinearGradient(
+          begin: Alignment(7.782464450656335e-9,-1.0542986392974854),
+          end: Alignment(1.0542986392974854,3.2185917918771487e-16),
+          colors: [Color.fromRGBO(65, 22, 156, 1),Color.fromRGBO(50, 49, 116, 1)]
+        ),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 63,
+        left: 37,
+        child: Text('Register new account', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'GT Walsheim Pro',
+        fontSize: 28,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1.5 /*PERCENT not supported*/
+      ),)
+      ),Positioned(
+        top: 131,
+        left: 33,
+        child: null
+      ),
+        ]
+      )
+    )
