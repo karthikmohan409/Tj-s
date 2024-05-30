9565e81259eb7379ed9260eda06f336c6cd854c6
karthikmohan409 <karthikmohan409@gmail.com>
2024-05-30 17:24:32 -0400

    Create screen1.dart

---
+++ screen1.dart
@@ -0,0 +1,64 @@
+      // Figma Flutter Generator Iphone1415promax1Widget - FRAME
+      Container(
+      width: 430,
+      height: 932,
+      decoration: BoxDecoration(
+          gradient : LinearGradient(
+          begin: Alignment(7.782464450656335e-9,-1.0542986392974854),
+          end: Alignment(1.0542986392974854,3.2185917918771487e-16),
+          colors: [Color.fromRGBO(65, 22, 156, 1),Color.fromRGBO(50, 49, 116, 1)]
+        ),
+      image : DecorationImage(
+          image: AssetImage('assets/images/Iphone1415promax1.png'),
+          fit: BoxFit.fitWidth
+      ),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: -0.00003814697265625,
+        left: 0,
+        child: Container(
+      width: 430.0000915527344,
+      height: 932.0000610351562,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0.00003759179526241496,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/rectangle45.svg',
+        semanticsLabel: 'rectangle45'
+      );
+      ),Positioned(
+        top: 932.0000610351562,
+        left: 430,
+        child: Transform.rotate(
+        angle: 179.99999499104388 * (math.pi / 180),
+        child: SvgPicture.asset(
+        'assets/images/rectangle46.svg',
+        semanticsLabel: 'rectangle46'
+      );,
+      )
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 274,
+        left: 36,
+        child: Container(
+        width: 368,
+        height: 364,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Whatsappimage20240515at113.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),
+        ]
+      )
+    )
